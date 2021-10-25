7b9620b777fc47a2345f0eaf51e6d9e74116e799
JackBB1972 <htech.by@gmail.com>
2021-10-25 11:26:49 +0300

    1.03 ver with English & Russian

---
+++ upload/admin/language/en-gb/extension/module/jslider.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']         = 'JSlider :: slideshow';
+
+// Text
+$_['text_extension']        = 'Extensions';
+$_['text_success']          = 'Settings changed successfully!';
+$_['text_edit']             = 'Module settings';
+$_['text_settings']         = 'Display Output Settings';
+$_['text_valueR']           = 'Present value';
+
+// Mode text
+$_['text_modeVertcal']      = 'Vertical change : ON';
+$_['text_modeHorizontal']   = 'Horizontal change : ON';
+$_['text_modeFade']         = 'Fade : ON';
+$_['text_modeVertcalOff']      = 'Vertical change';
+$_['text_modeHorizontalOff']   = 'Horizontal change';
+$_['text_modeFadeOff']         = 'Fade';
+
+// Entry
+$_['entry_name']            = 'Module name';
+$_['entry_title']           = 'Title';
+$_['entry_display_title']   = 'Display title?';
+$_['entry_description']     = 'Text';
+$_['entry_status']          = 'Status';
+$_['entry_banner']          = 'Select a group of banners';
+$_['entry_addspan']         = 'Wrap header content in span';
+$_['entry_header_class']    = 'CSS heading class';
+$_['entry_mode']            = 'Shift option: vertical, horizontal or fade';
+$_['entry_speed']           = 'Effect duration in milliseconds';
+$_['entry_navi']            = 'Show navigation';
+$_['entry_capt']        = 'Slide captions. From image attributes.';
+
+// Error
+$_['error_permission']      = 'You do not have permission to manage this module!';
+$_['error_name']            = 'Module name must be between 3 and 64 characters!';
+
